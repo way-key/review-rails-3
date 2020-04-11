ba75c29f37c4ecf3c720b5e318fd019bb55638d3
way-key <45gqz7@gmail.com>
2020-04-11 12:38:18 +0900

    [Start,Finish]4/8~4/11

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '5d32415bf3660bd5b3ab3e529d3356c85febd3dfa41c00a8366e43f0c3327631ed738a48573fd67490a3514ef86ff72ac0b1b7a0fe10a368120d5fecaa522aaa'
